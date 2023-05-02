1adaebbf5ace63f884c46184d5b400b9a58a7be8
Victor Pozo Fernandez <108496792+VictorPozoFernandez@users.noreply.github.com>
2023-05-02 12:08:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ model = Blip2ForConditionalGeneration.from_pretrained(
 model.to(device)
 
 def generate_text(image: Image.Image) -> str:
-    inputs = processor(images=image, return_tensors="pt").to(device, torch.float16)
+    prompt = "Question: Give me a detailed visual description of the object. Answer:"
+    inputs = processor(images = image, text=prompt, return_tensors="pt").to(device, torch.float16)
     generated_ids = model.generate(**inputs)
     generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
     return generated_text
