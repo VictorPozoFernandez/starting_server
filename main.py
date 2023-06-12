d1897ec12c4bbaa3d660a1b8090f4a664cacf62b
Victor Pozo Fernandez <108496792+VictorPozoFernandez@users.noreply.github.com>
2023-06-12 11:43:45 +0200

    Update main.py

--- main.py
+++ main.py
@@ -95,6 +95,8 @@ async def annotate_image(text_prompt: str, photo: UploadFile = File(...)):
     segmented_image= cv2.imread("segmented_image2.png", cv2.IMREAD_UNCHANGED)
     cut_image = segmented_image[boxes_xyxy[1]:boxes_xyxy[3], boxes_xyxy[0]:boxes_xyxy[2]]
     current_height, current_width = cut_image.shape[:2]
+    print(current_height*current_width)
+
     
     # Calculate the desired size
     if current_height*current_width < 15000:
@@ -105,28 +107,40 @@ async def annotate_image(text_prompt: str, photo: UploadFile = File(...)):
         desired_size = (int(1.5*current_width), int(1.5*current_height))
     elif current_height*current_width < 45000:
         desired_size = (int(1.25*current_width), int(1.25*current_height))
+    else:
+        desired_size = (int(current_width), int(current_height))
 
     resized_image = cv2.resize(cut_image, desired_size)
     imageio.imwrite('final_image.png', resized_image)
     
-    #Embbeding image (CLIP)
-    final_image= cv2.imread("final_image.png")
-    inputs = processor(images=final_image, return_tensors="pt", padding=True).to(DEVICE)
-    image_emb = model_clip.get_image_features(**inputs)
-    image_emb = image_emb.squeeze(0).cpu().detach().numpy()
+    # Loop over the angles
+    for angle in range(0, 360, 25):
     
-    #Querying Pinecone (the response varies depending on the searched namespace)
-    pinecone.init(api_key="04d65f27-278f-40f0-9cfb-c907b84115a7", environment="us-east4-gcp")
-    index = pinecone.Index("text-embeddings1")
+        final_image= cv2.imread("final_image.png")
+        image_center = tuple(np.array(final_image.shape[1::-1]) / 2)
+        rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+        rotated_image = cv2.warpAffine(final_image, rot_mat, final_image.shape[1::-1], flags=cv2.INTER_LINEAR)
+        
+        #Embbeding image (CLIP)
+        inputs = processor(images=rotated_image, return_tensors="pt", padding=True).to(DEVICE)
+        image_emb = model_clip.get_image_features(**inputs)
+        image_emb = image_emb.squeeze(0).cpu().detach().numpy()
 
-    query_response = index.query(
-        namespace="example-namespace",
-        top_k=10,
-        include_metadata=True,
-        vector=image_emb.tolist())
+        #Querying Pinecone (the response varies depending on the searched namespace)
+        pinecone.init(api_key="04d65f27-278f-40f0-9cfb-c907b84115a7", environment="us-east4-gcp")
+        index = pinecone.Index("text-embeddings1")
 
-    print(query_response.matches)
-    filtered_ids = filter_top_scores(query_response.matches)
+        query_response = index.query(
+            namespace="example-namespace",
+            top_k=10,
+            include_metadata=True,
+            vector=image_emb.tolist())
+
+        print(query_response.matches)
+        filtered_ids = filter_top_scores(query_response.matches)
+        
+        if filtered_ids != "Element not identified":
+            return filtered_ids
     
     return filtered_ids
 
@@ -181,7 +195,7 @@ def filter_top_scores(input_list):
     # Find the element with the highest score
     highest_score_element = max(input_list, key=lambda x: x['score'])
 
-    if highest_score_element["score"] < 0.57:
+    if highest_score_element["score"] < 0.60:
         answer = "Element not identified"
         return answer
 
@@ -204,4 +218,3 @@ def filter_top_scores(input_list):
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
-
